c18ee773c86d5e97284f78fa4bcc954a5648b62e
Luz  Restrepo <106494261+luzrestrepo@users.noreply.github.com>
2022-11-03 22:10:55 -0500

    Autores

---
+++ autores/client/src/App.js
@@ -0,0 +1,23 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import ActualizarAutor from './componentes/ActualizarAutor';
+import Error from './componentes/Error';
+import NuevoAutor from './componentes/NuevoAutor';
+import TodosAutores from './componentes/TodosAutores';
+
+const App = () => {
+  return(
+    <div className="container">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact render={()=><TodosAutores />} />
+          <Route path="/nuevo"  render={()=><NuevoAutor/>} />
+          <Route path="/autor/editar/:id"  render={()=><ActualizarAutor/>} />
+          <Route path="/error"  render={()=><Error/>} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App;
+
---
+++ autores/client/src/componentes/TodosAutores.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const TodosAutores = () => {
+
+    const [autores, setAutores] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/autores")
+            .then(res => setAutores(res.data))
+            .catch(err => console.log(err));
+    }, [])
+
+    const borrarAutor = id => {
+        axios.delete("http://localhost:8000/api/autores/"+id)
+        .then(res => {
+            //actualizar la lista de autores por medio de filter 
+            let nuevaLista = autores.filter(autor => autor._id != id);
+            setAutores(nuevaLista)
+        })
+    
+    }
+    return (
+        <div>
+            <h1>Autores</h1>
+            <Link to="/nuevo" className="btn btn-success">Nuevo Autor</Link>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Imagen</th>
+                        <th>Libros</th>
+                        <th>Cuentos</th>
+                        <th>Artículos</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                        autores.map((autor, index) => (
+                            <tr key={index}>
+                                <td>{autor.nombre}</td>
+                                <td>
+                                    <img src={autor.imagen} alt="autor" className="img-fluid" />
+                                </td>
+                                <td>
+                                    {
+                                        autor.libros ? <span className="bi bi-check text-success"></span> : <span className="bi bi-x text-danger"></span>
+                                    }
+                                </td>
+                                <td>
+                                    {
+                                        autor.cuentos ? <span className="bi bi-check text-success"></span> : <span className="bi bi-x text-danger"></span>
+                                    }
+                                </td>
+
+                                <td>
+                                        {
+                                            autor.articulos ? <span className="bi bi-check text-success"></span> : <span className="bi bi-x text-danger"></span>
+                                        }
+                                    </td>
+                                    <td>
+                                        <Link className="btn btn-warning" to={`/autor/editar/${autor._id}`}>Editar</Link>
+                                        <button className="btn btn-danger" onClick={() =>
+                                         borrarAutor(autor._id)} >Eliminar</button>
+                                    </td>
+                                </tr>
+            ))
+                    }
+        </tbody>
+            </table >
+        </div >
+    )
+
+}
+
+export default TodosAutores;
+
+
+
